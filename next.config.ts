ef6453fdfd0842c8e0e11822934ce5112afedf5e
huzaechxx <huzaifazahid87@gmail.com>
2025-01-23 16:30:37 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  "scripts": {
+    "build:ignore-lint": "next build && eslint . --quiet --fix || true"
+  },
 
   async redirects() {
     return [
