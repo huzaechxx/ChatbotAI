3aff714d35c836ae6ad05da0efb7df7a4f280e21
huzaechxx <huzaifazahid87@gmail.com>
2025-02-02 15:57:25 +0500

    Update login.ts

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -18,25 +18,30 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     if (req.method === 'POST') {
     const { email, password } = req.body;
+    console.log(req.body);
     const currentuser = extractUsername(email);
+    console.log(currentuser);
+    
     if (!email || !password) {
       return res.status(400).json({ message: 'Email and password are required' });
     }
-
+    console.log("not connected");
+    
     await connectToDatabase();
-
+    console.log("ohoho connected");
     const user = await User.findOne({ email }).select('email password');
+    console.log("user found");
     if (!user || !(await bcrypt.compare(password, user.password))) {
       return res.status(401).json({ message: 'Invalid credentials' });
     }    
-
+    console.log("token not generated");
     const token = jwt.sign({ email: user.email, id: user._id }, process.env.JWT_SECRET as string, {
       expiresIn: '1h',
     });
-
+    console.log("token generated");
     res.status(200).json({ token,currentuser });
     res.setHeader('Location', '/chat');
   } else {
-    res.status(405).json({ message: 'Method not allowed' });
+    res.status(405).json({ message: 'Method not allowed hehe' });
   }
 }
